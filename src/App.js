05c86baac1f6919766b5f1c823c81869c48af6a2
이다영 <id46827@gmail.com>
2024-07-23 23:23:42 +0900

    기존 기능 삭제 및 dialog태그 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import styled from "styled-components";
 import "./App.css";
-import { useState } from "react";
+import { useRef } from "react";
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false);
+  const modalRef = useRef();
 
   return (
     <Container>
-      <ShowBtn onClick={() => setModalOpen(true)}>Modal open</ShowBtn>
-      {modalOpen && (
-        <BlackBg onClick={() => setModalOpen(false)}>
-          <ModalBox>
-            <Modal>
-              <h1>Modal</h1>
-              <button onClick={() => setModalOpen(false)}>Close</button>
-            </Modal>
-          </ModalBox>
-        </BlackBg>
-      )}
+      <ShowBtn onClick={() => modalRef.current.showModal()}>Modal open</ShowBtn>
+
+      <dialog ref={modalRef}>
+        <h1>Modal</h1>
+        <HideBtn onClick={() => modalRef.current.close()}>Close</HideBtn>
+      </dialog>
     </Container>
   );
 }
@@ -27,50 +22,14 @@ export default App;
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-`;
 
-const BlackBg = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
-  position: relative;
-`;
-
-const ModalBox = styled.div`
-  width: 500px;
-  height: 300px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const Modal = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  border-radius: 5px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
-
-  h1 {
-    color: #333;
-  }
-
-  button {
+  dialog {
+    width: 500px;
+    height: 200px;
     border: none;
     border-radius: 5px;
-    background-color: #40a578;
-    padding: 10px 20px;
-    color: #fff;
-    cursor: pointer;
-    margin-bottom: 20px;
-  }
-  button:active {
-    background-color: #40a578dc;
+    text-align: center;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
   }
 `;
 
@@ -88,6 +47,20 @@ const ShowBtn = styled.button`
   font-size: 20px;
   letter-spacing: 2px;
   font-weight: bold;
+
+  &:active {
+    background-color: #40a578dc;
+  }
+`;
+
+const HideBtn = styled.button`
+  border: none;
+  border-radius: 5px;
+  background-color: #40a578;
+  padding: 10px 15px;
+  color: #fff;
+  cursor: pointer;
+  margin-top: 60px;
 
   &:active {
     background-color: #40a578dc;
